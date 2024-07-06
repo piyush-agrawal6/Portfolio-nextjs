5c3601f0c2c4a987b209d2d5352f0ffe68bdd868
Piyush Agrawal <agrawaljoy1@gmail.com>
2024-07-06 13:30:30 +0530

    navbar completed

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -24,11 +24,26 @@ const MobileNav = () => {
         <div className="mt-20 mb-20 text-center text-2xl">
           <Link href="/">
             <h1 className="text-4xl font-semibold">
-              CodeBuff<span className="text-accent">.</span>
+              Piyush<span className="text-accent">.</span>
             </h1>
           </Link>
         </div>
-        <nav>Nav</nav>
+        <nav className="flex flex-col gap-8 justify-center items-center">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
